8cb24906c12e304ee37b9723e30aba284fe8be66
geovannaacunha <rm97736@fiap.com.br>
2023-03-07 11:49:13 -0300

    script.js adicionado ao html.

---
+++ JS/script.js
@@ -0,0 +1,13 @@
+//Este é um comentário de linha
+
+/**
+ * E este é 
+ * um comentário
+ * de bloco
+ */
+
+//Exemplo das funções do console.
+console.log("Olá mundo!")
+console.error("Bom dia")
+console.warn("Será?")
+console.info("Vai ser...")
